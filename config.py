78ee269a7ac7df78320b685f8a52308a9c0fbce8
Lim <151501654+beautyjie@users.noreply.github.com>
2024-09-01 16:47:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cxe1tiyimjuy.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limyijie-employee"
+customregion = "US East (N. Virginia) us-east-1"
+
